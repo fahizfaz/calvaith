2ebee9df382e02d85d656da52d92ee115e88a617
fahizfaz <90083393+fahizfaz@users.noreply.github.com>
2026-02-03 19:15:59 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,3 +98,28 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const faqItems = document.querySelectorAll('.faq-item');
+
+  faqItems.forEach(item => {
+    const button = item.querySelector('.faq-question');
+    const icon = item.querySelector('.faq-icon');
+
+    button.addEventListener('click', () => {
+      faqItems.forEach(i => {
+        i.classList.remove('active');
+        i.querySelector('.faq-icon').textContent = '+';
+      });
+
+      item.classList.add('active');
+      icon.textContent = '−';
+    });
+  });
+
+
+});
+
+
